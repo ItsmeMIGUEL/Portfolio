8e9337b392445179342fe3a3e6dc8b68028c0b5e
JM Arcuino <122060931+ItsmeMIGUEL@users.noreply.github.com>
2025-03-27 20:31:01 +0800

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 const firebaseConfig = {
-  apiKey: "AIzaSyBkvBOrAfPifpBGo3QV3_wKP3pmBK6gzpg",
-  authDomain:"portfolio-82ad6.firebaseapp.com",
-  projectId:"portfolio-82ad6",
-  storageBucket: "portfolio-82ad6.firebasestorage.app",
-  messagingSenderId: "910171044987",
-  appId: "1:910171044987:web:974692ecd6f2b8e7ef36de",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: "G-KT0XF48PVS",
 };
 const app = initializeApp(firebaseConfig);
